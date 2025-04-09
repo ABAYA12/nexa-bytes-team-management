5d54c8e4bbb44a7861b712749fd90a9e2fb1bbe1
Ishmael Kabu Abayateye <123239306+ABAYA12@users.noreply.github.com>
2025-04-09 14:49:00 +0000

    added all file

--- supabase/config.js
+++ supabase/config.js
@@ -1,6 +1,6 @@
 // Supabase configuration
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_PUBLIC_KEY';
+const supabaseUrl = 'DB_URL = postgresql://postgres.smzezqtlqndrbesykjoo:RiskGuard AI@aws-0-us-east-2.pooler.supabase.com:5432/postgres';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtemV6cXRscW5kcmJlc3lram9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNDE0MDAsImV4cCI6MjA1NzcxNzQwMH0.RCzrsJ9dnvPaDiOa1g8zDqBo9WaiqZLdDtabm9fMBtw';
 
 // Initialize Supabase client
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
